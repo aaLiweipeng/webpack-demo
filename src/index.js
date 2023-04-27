562b2feebd3566da8e6481973084b80657454b12
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-04-28 01:48:50 +0800

    动态引入_懒加载

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 /*
- * @Descripttion: 
+ * @Descripttion:
  * @Author: lwp
  * @Date: 2023-04-17 02:52:22
- * @LastEditTime: 2023-04-28 01:43:39
+ * @LastEditTime: 2023-04-28 01:48:32
  */
 // 引入 css
-import css1 from './style/style1.css'
+import css1 from "./style/style1.css";
 import less2 from "./style/style2.less";
 console.log("css1", css1);
 console.log("less2", less2);
@@ -13,10 +13,10 @@ console.log("less2", less2);
 import { sum } from "./math";
 import _ from "lodash";
 
-import { sum } from './math'
+import { sum } from "./math";
 
-const sumRes = sum(10, 20)
-console.log('sumRes', sumRes)
+const sumRes = sum(10, 20);
+console.log("sumRes", sumRes);
 
 // // 增加，开启热更新之后的代码逻辑
 // if (module.hot) {
@@ -26,16 +26,23 @@ console.log('sumRes', sumRes)
 //     })
 // }
 
-console.log('window.ENV', ENV)
+console.log("window.ENV", ENV);
 
 // 引入图片
 function insertImgElem(imgFile) {
-    const img = new Image() // 新建一个img标签实例
-    img.src = imgFile // 给img实例的src属性赋值
-    document.body.appendChild(img) // 把img标签加到html正文
+  const img = new Image(); // 新建一个img标签实例
+  img.src = imgFile; // 给img实例的src属性赋值
+  document.body.appendChild(img); // 把img标签加到html正文
 }
 
-import imgFile1 from './img/1.png'
-insertImgElem(imgFile1)
-import imgFile2 from './img/2.jpeg'
-insertImgElem(imgFile2)
+import imgFile1 from "./img/1.png";
+insertImgElem(imgFile1);
+import imgFile2 from "./img/2.jpeg";
+insertImgElem(imgFile2);
+
+// 动态引入数据 —— 懒加载
+setTimeout(() => {
+  import("./dynamic-data.js").then((res) => {
+    console.log(res.default.message);
+  });
+}, 1500);
